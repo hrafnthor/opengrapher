c19ad13cfec85ab3e9f9f7b8ff67cf94ed6c4d8e
Hrafn Thorvaldsson <hrafn@hth.is>
2023-02-07 16:06:49 +0000

    skrapeit and kotlin-result libraries added

--- opengraph-fetch/library/build.gradle.kts
+++ opengraph-fetch/library/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
 }
 
 dependencies {
+    implementation(libs.skrapeit)
+    implementation(libs.michaelbull.result)
+
     testImplementation(libs.kotest.runner.junit5)
     testImplementation(libs.kotest.assertions.core)
     testImplementation(libs.junit.jupiter)
