2115c52b1627f2a40ef688168457e62ebe69cd1f
Hrafn Thorvaldsson <hrafn@hth.is>
2023-02-10 10:00:26 +0000

    tree parsing finalized

--- opengraph-fetch/library/src/main/kotlin/is/hth/opengrapher/tree/Tree.kt
+++ opengraph-fetch/library/src/main/kotlin/is/hth/opengrapher/tree/Tree.kt
@@ -1,37 +1,83 @@
 package `is`.hth.opengrapher.tree
 
-
 internal fun add(key: String, value: String, root: Node) {
-    val keys: MutableList<String> = key.lowercase().split(":").toMutableList()
-    val firstKey = keys.removeFirstOrNull() ?: ""
-    if (firstKey != "og") {
-        throw IllegalArgumentException("Property key did not start with required prefix 'og:' !")
+    if (key.isNotBlank() && openGraphSpecification.contains(key)) {
+        add(key.removePrefix("og:").split(":").toMutableList(), value, root)
     }
-    add(keys, value, root)
 }
 
-private fun add(keys: MutableList<String>, value: String, node: Node) {
-    val firstKey = getKey(keys)
-    val childIndex = if (firstKey.isNotBlank()) node.children.indexOfFirst { it.key == firstKey } else -1
+private fun add(keys: MutableList<String>, value: String, parent: Node) {
+    val keySegment = keys.removeFirstOrNull() ?: return
+
     when {
-        childIndex == -1 && keys.isEmpty() -> node.children.add(Node(key = firstKey, value = value))
-        childIndex >= 0 -> add(keys, value, node.children[childIndex])
+        keys.isEmpty() && parent.key == keySegment -> parent.value = value
+        keys.isEmpty() -> {
+            when {
+                keySegment.matches(mediaRootRegex) -> "url"
+                keySegment.matches(localeRootRegex) -> "default"
+                keySegment.matches(localeAlternateRegex) -> "value"
+                else -> null
+            }?.apply(keys::add)
+
+            val child = Node(key = keySegment, value = "").apply(parent.children::add)
+            if (keys.isEmpty()) {
+                child.value = value
+            } else {
+                add(keys, value, child)
+            }
+        }
+
         else -> {
-            val child = Node(key = firstKey, value = "")
+            val child = parent.children.lastOrNull { it.key == keySegment } ?: Node(
+                key = keySegment,
+                value = ""
+            ).apply(parent.children::add)
             add(keys, value, child)
-            node.children.add(child)
         }
     }
 }
 
-private val mediaRootRegex: Regex = "^(image|video|audio)".toRegex()
 
-private fun getKey(keys: MutableList<String>): String {
-    val firstKey = keys.removeFirstOrNull() ?: ""
-    if (keys.isEmpty() && firstKey.matches(mediaRootRegex)) {
-        // Special case where media root tags (image, video, audio) contain the 'url' content directly
-        // rather than using a leaf tag (i.e. og:image:url).
-        keys.add("url")
-    }
-    return firstKey
-}
+private val mediaRootRegex: Regex = "^(image|video|audio)\$".toRegex()
+private val localeRootRegex: Regex = "^locale\$".toRegex()
+private val localeAlternateRegex: Regex = "^alternate\$".toRegex()
+
+/**
+ * The Open Graph Protocol specification as described on https://ogp.me.
+ */
+private val openGraphSpecification: Set<String> = setOf(
+    // Required
+    "og:title",
+    "og:type",
+    "og:url",
+    "og:image",
+
+    // Optional
+    "og:audio",
+    "og:description",
+    "og:determiner",
+    "og:locale",
+    "og:locale:alternate",
+    "og:site_name",
+    "og:video",
+
+    // Structured Property - Image
+    "og:image:url",
+    "og:image:secure_url",
+    "og:image:type",
+    "og:image:width",
+    "og:image:height",
+    "og:image:alt",
+
+    // Structured Property - Video
+    "og:video:url",
+    "og:video:secure_url",
+    "og:video:type",
+    "og:video:width",
+    "og:video:height",
+
+    // Structured Property - Audio
+    "og:audio:url",
+    "og:audio:secure_url",
+    "og:audio:type",
+)
