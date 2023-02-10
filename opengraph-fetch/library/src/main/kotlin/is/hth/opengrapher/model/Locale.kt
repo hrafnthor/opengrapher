c168174d4f26e9887974418fa2fbb857d9d624db
Hrafn Thorvaldsson <hrafn@hth.is>
2023-02-10 10:05:03 +0000

    refactored model field naming and removed unused fields

--- opengraph-fetch/library/src/main/kotlin/is/hth/opengrapher/model/Locale.kt
+++ opengraph-fetch/library/src/main/kotlin/is/hth/opengrapher/model/Locale.kt
@@ -1,7 +1,6 @@
 package `is`.hth.opengrapher.model
 
-
 public data class Locale(
     val default: String = "",
-    val alternatives: List<String> = emptyList()
+    val alternate: List<String> = emptyList()
 )
--- opengraph-fetch/library/src/main/kotlin/is/hth/opengrapher/model/Video.kt
+++ opengraph-fetch/library/src/main/kotlin/is/hth/opengrapher/model/Video.kt
@@ -6,7 +6,4 @@ public data class Video(
     val type: String = "",
     val width: Int = -1,
     val height: Int = -1,
-    val duration: Int = -1,
-    val releaseDate: String = "",
-    val series: String = "",
 )
